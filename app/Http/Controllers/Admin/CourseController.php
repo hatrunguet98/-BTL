7789cf2a2c169a9ed84c244725515311fffb36fa
Ha Tu <hatu98nd@gmail.com>
2018-12-14 00:58:25 +0700

    tao chức năng thêm môn học

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -9,6 +9,7 @@ use App\Subject;
 use App\Semester;
 use Illuminate\Support\Facades\DB;
 use App\Services\ClassAdmin\ClassQueryUser;
+use Illuminate\Support\Facades\Auth;
 
 class CourseController extends Controller
 {
@@ -21,6 +22,40 @@ class CourseController extends Controller
     }
 
     public function addCourse(Request $request){
-    	dd($request);
+        $data = $request->all();
+        $semesters = DB::table('semesters')
+                    ->where('id',$data['semester'])
+                    ->first();
+        $subject = DB::table('subjects')
+                    ->where('id', $data['code'])
+                    ->first();
+        $course = DB::table('courses')
+                    ->where('subject_id', $data['code'])
+                    ->orderBy('id', 'desc')
+                    ->first();
+        $admin_id = Auth::user()->id;
+
+        if($subject) {
+            $code = "";
+            if(!$course){
+                $code = $subject->code . " 1";
+                
+            } else {
+                $code = $subject->code ." ".(substr($course->code,8) + 1);
+            }
+            $subject_id = $subject->id;
+            $subject_name = $subject->name;
+            $semester = $semesters->name;
+            DB::table('courses')->insert([
+                'admin_id' => $admin_id,
+                'code' => $code,
+                'subject_id' => $subject_id,
+                'name' => $subject_name,
+                'semester' => $semester,
+            ]);
+        } else {
+            dd("@@@");
+        }
+       return redirect('/course');
     }
 }
