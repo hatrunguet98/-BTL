18bb3d0a8f5c172ffb11032f4f523c3becf5847b
NGUYEN TIEN VIET <127phudu@gmail.com>
2018-12-29 13:50:56 +0700

    Sửa required form insert course

--- resources/views/admin/courses/InsertSingleCourseModal.blade.php
+++ resources/views/admin/courses/InsertSingleCourseModal.blade.php
@@ -4,13 +4,13 @@
         <div class="modal-content">
             <div class="form">
                 <h2>Thêm mới lớp môn học</h2>
-                <form action="{{ url('add-course') }}" method="post">
+                <form action="{{ url('add-course') }}" method="post" id="insert-course">
                     @csrf
                     <div class="form-group col-md-6" style="z-index: 2">
                         <div class="input-group">
                             <span class="input-group-addon "><i class="fa fa-book" aria-hidden="true"></i></span>
-                            <select class="selectpicker form-control bootstrap-select" title="Chọn mã môn học" data-count-selected-text="Not changable text" name="code" id="select-code" data-live-search="true">
-                                <option id="code0" selected disabled style="display: none;">Chọn mã môn học</option>
+                            <select class="selectpicker form-control bootstrap-select" name="code" id="select-code" data-live-search="true" required>
+                                <option id="code0" selected disabled style="display: none;" value="">Chọn mã môn học</option>
                                 @foreach($subjects as $subject)
                                     <option id="{{'code' . $subject->id}}"  value="{{$subject->id}}">{{$subject->code}}</option>
                                 @endforeach
@@ -20,8 +20,8 @@
                     <div class="form-group col-md-6" style="z-index: 2">
                         <div class="input-group">
                             <span class="input-group-addon"><i class="fa fa-book" aria-hidden="true"></i></span>
-                            <select class="selectpicker  form-control bootstrap-select" name="semester" data-live-search="true">
-                                <option selected disabled style="display: none">Chọn mã học kỳ</option>
+                            <select class="selectpicker  form-control bootstrap-select" name="semester" id="select-semester" data-live-search="true" required>
+                                <option selected disabled style="display: none" value="">Chọn mã học kỳ</option>
                                 @foreach($semesters as $semester)
                                     <option id="{{'semester' . $semester->id}}"  value="{{$semester->id}}">{{$semester->name}}</option>
                                 @endforeach
@@ -31,8 +31,8 @@
                     <div class="form-group col-md-6" style="z-index: 1">
                         <div class="input-group">
                             <span class="input-group-addon"><i class="fa fa-book" aria-hidden="true"></i></span>
-                            <select class="selectpicker  form-control bootstrap-select" name="subject" id="select-subject" data-live-search="true">
-                                <option id="name0" selected disabled style="display: none">Chọn tên môn học</option>
+                            <select class="selectpicker  form-control bootstrap-select" name="subject" id="select-subject" data-live-search="true" required>
+                                <option id="name0" selected disabled style="display: none" value="">Chọn tên môn học</option>
                                 @foreach($subjects as $subject)
                                     <option id="{{'name' . $subject->id}}" value="{{$subject->id}}">{{$subject->name}}</option>
                                 @endforeach
@@ -42,8 +42,8 @@
                     <div class="form-group col-md-6" style="z-index: 0">
                         <div class="input-group">
                             <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
-                            <select class="selectpicker form-control bootstrap-select"  name="user" data-live-search="true">
-                                <option selected disabled style="display: none;">Chọn tên giảng viên</option>
+                            <select class="selectpicker form-control bootstrap-select"  name="user"  id="select-teacher" data-live-search="true" required>
+                                <option selected disabled style="display: none;" value="">Chọn tên giảng viên</option>
                                 @foreach($teachers as $teacher)
                                     <option id="{{'user' . $teacher->id}}" value="{{$teacher->id}}">{{$teacher->name}}</option>
                                 @endforeach
@@ -52,7 +52,7 @@
                     </div>
 
 
-                    <button type="submit" class="btn btn-primary submitBtn">Submit</button>
+                    <button type="submit" id="submit-course" class="btn btn-primary submitBtn">Submit</button>
                     <button type="button" class="btn btn-danger closeBtn" data-dismiss="modal">Close</button>
                 </form>
             </div>
